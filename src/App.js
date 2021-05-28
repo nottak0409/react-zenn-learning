5016cc424899f54220c7543b6ff23e29993a7e22
nottak0409 <nottak0409@gmail.com>
2021-05-29 08:09:38 +0900

    JSX fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 function App() {
     return (
         <div>
-            <h1>Hello, World!</h1>
-        </div>
-    );
+            <header>
+                <h1>Cute Dogs Images</h1>
+            </header>
+            <main>
+                <section>
+                <figure>
+                  <img
+                    src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+                    alt="cute dog"
+                  />
+                </figure>
+              </section>
+            </main>
+            <footer>
+              <p>Dog images are retrieved from Dog API</p>
+              <p>
+                <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+              </p>
+            </footer>
+          </div>    );
 }
 
 export default App;
